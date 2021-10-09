be41a7118327d8ac7558031916184a7f18b6acc6
Craig Morris <craig.michael.morris@gmail.com>
2021-10-09 08:38:36 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="tap-gocardless",
+    version="0.1.0",
+    description="Singer.io tap for extracting data",
+    author="Stitch",
+    url="http://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_gocardless"],
+    install_requires=[
+        # NB: Pin these to a more specific version for tap reliability
+        "singer-python",
+        "requests",
+    ],
+    entry_points="""
+    [console_scripts]
+    tap-gocardless=tap_gocardless:main
+    """,
+    packages=["tap_gocardless"],
+    package_data = {
+        "schemas": ["tap_gocardless/schemas/*.json"]
+    },
+    include_package_data=True,
+)
